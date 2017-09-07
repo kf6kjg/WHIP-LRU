959335d43173e2613c42ec3c23dc7aedb978c1fb
Ricky Curtice <kf6kjg@users.noreply.github.com>
2017-09-06 18:59:57 -0700

    Client requests should now decode

--- Source/WHIP_LRU/Server/ClientRequestMsg.cs
+++ Source/WHIP_LRU/Server/ClientRequestMsg.cs
@@ -22,15 +22,74 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using System;
 using System.Collections.Generic;
+using System.Linq;
+using OpenMetaverse;
+using InWorldz.Whip.Client;
+using System.Text;
 
 namespace WHIP_LRU.Server {
 	public class ClientRequestMsg {
-		private const int HEADER_SIZE = 37;
-		private const int DATA_SIZE_MARKER_LOC = 33;
+		private const short DATA_SIZE_MARKER_LOC = 33;
+		private const short HEADER_SIZE = 37;
+		private const short UUID_TAG_LOCATION = 1;
+		private const short UUID_LEN = 32;
+		private readonly List<byte> _data = new List<byte>();
 
-		private enum RequestType {
+		public byte[] Data => _data.Skip(HEADER_SIZE).ToArray();
+		public UUID Id { get; private set; }
+		public bool IsReady { get; private set; }
+		public RequestType Type { get; private set; }
+
+		private string GetHeaderSummary() {
+			var intStatus = (int)_data[0];
+			var dataSize = InWorldz.Whip.Client.Util.NTOHL(_data.Take(HEADER_SIZE).ToArray(), DATA_SIZE_MARKER_LOC);
+			return " Status: " + intStatus + " Size was: " + dataSize;
+		}
+
+		public bool AddRange(IEnumerable<byte> data) {
+			if (!IsReady) { // Refuse to append more data once loaded.
+				_data.AddRange(data);
+
+				if (_data.Count >= HEADER_SIZE) {
+					var header = _data.Take(HEADER_SIZE).ToArray();
+
+					// We've enough of the header to determine size.
+					var dataSize = InWorldz.Whip.Client.Util.NTOHL(header, DATA_SIZE_MARKER_LOC);
+					var packetSize = HEADER_SIZE + dataSize;
+
+					if (packetSize > 0 && _data.Count >= packetSize) {
+						// Load the class up with the data.
+						var type = header[0];
+						if (typeof(RequestType).IsEnumDefined(type)) {
+							Type = (RequestType)type;
+						}
+						else {
+							throw new AssetProtocolError("Invalid result type in server response: " + GetHeaderSummary());
+						}
+
+						var idBytes = header.Skip(UUID_TAG_LOCATION).Take(UUID_LEN);
+						var encoding = new ASCIIEncoding();
+						var idString = encoding.GetString(idBytes.ToArray());
+						UUID id;
+						if (UUID.TryParse(idString, out id)) {
+							Id = id;
+						}
+						else {
+							throw new AssetProtocolError("Invalid UUID in server response: " + GetHeaderSummary());
+						}
+
+						IsReady = true;
+
+						// If all the expected data has arrived, return true, else false.
+					}
+				}
+			}
+
+			return IsReady;
+		}
+
+		public enum RequestType {
 			RT_GET = 10,
 			RT_PUT = 11,
 			RT_PURGE = 12,
@@ -38,20 +97,7 @@ namespace WHIP_LRU.Server {
 			RT_MAINT_PURGELOCALS = 14,
 			RT_STATUS_GET = 15,
 			RT_STORED_ASSET_IDS_GET = 16,
-			RT_GET_DONTCACHE = 17
+			RT_GET_DONTCACHE = 17,
 		};
-
-		public ClientRequestMsg() {
-		}
-
-		private List<byte> _data = new List<byte>();
-		public bool AddRange(IEnumerable<byte> data) {
-			_data.AddRange(data);
-
-			// TODO: determine if the added range completes the message or not.
-
-			return true;
-		}
-
 	}
 }
