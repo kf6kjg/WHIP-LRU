85781dc60c350f54b14d20d7deb5e913945eb313
Ricky C <kf6kjg@users.noreply.github.com>
2018-02-18 14:26:24 -0800

    Tests and fixes for the AddRange method of ClientRequestMessage
    Fixed several things:
    * An NRE if passed a null.  Not like it'd ever happen in practice without being a bug.  At least it's a little more explicit about what went wrong.
    * Fixed some C&P problems with error messages.
    
    And I changed it to support partial packets, even of the header.  This also solved a bug where the data would be rejected if you didn't send a large enough packet even after processing the header!

--- Source/LibWhipLru/Server/ClientRequestMsg.cs
+++ Source/LibWhipLru/Server/ClientRequestMsg.cs
@@ -61,9 +61,11 @@ namespace LibWhipLru.Server {
 		public byte[] Data { get => _data; }
 		private byte[] _data;
 		private int _dataWritePointer;
+		private readonly byte[] _dataHeader = new byte[HEADER_SIZE];
+		private int _dataHeaderWritePointer;
 
 		/// <summary>
-		/// Gets a value indicating whether this <see cref="T:LibWhipLru.Server.ClientRequestMsg"/> is ready.
+		/// Gets a value indicating whether this <see cref="T:LibWhipLru.Server.ClientRequestMsg"/> is has completed receiving all expected packet data.
 		/// </summary>
 		/// <value><c>true</c> if is ready; otherwise, <c>false</c>.</value>
 		public bool IsReady { get; private set; }
@@ -82,9 +84,7 @@ namespace LibWhipLru.Server {
 		/// <returns><c>true</c>, if all expected data is received, <c>false</c> otherwise.</returns>
 		/// <param name="data">Data.</param>
 		public bool AddRange(byte[] data) {
-			if (data.Length < HEADER_SIZE) { // Only allow packets big enough to contain the whole header.
-				throw new ArgumentOutOfRangeException(nameof(data), $"Data buffer MUST be at least {HEADER_SIZE} bytes long.");
-			}
+			data = data ?? throw new ArgumentNullException(nameof(data));
 
 			if (IsReady) { // Refuse to append more data once loaded.
 				throw new InvalidOperationException("You cannot reuse messages!");
@@ -92,44 +92,53 @@ namespace LibWhipLru.Server {
 
 			_bytesRead += data.Length;
 
-			if (!_typeIsReady && _bytesRead >= REQUEST_TYPE_LOC + 1) {
-				var type = data[REQUEST_TYPE_LOC];
+			var headerBytesInData = 0;
+
+			if (_dataHeaderWritePointer < HEADER_SIZE) {
+				// Incomplete header, so bring in what we've received, but nothing more than the header.
+				headerBytesInData = Math.Min(_dataHeader.Length - _dataHeaderWritePointer, data.Length);
+				Buffer.BlockCopy(data, 0, _dataHeader, _dataHeaderWritePointer, headerBytesInData);
+				_dataHeaderWritePointer += headerBytesInData;
+			}
+
+			if (!_typeIsReady && _dataHeaderWritePointer >= REQUEST_TYPE_LOC + 1) {
+				var type = _dataHeader[REQUEST_TYPE_LOC];
 				if (typeof(RequestType).IsEnumDefined((int)type)) {
 					Type = (RequestType)type;
 					_typeIsReady = true;
 				}
 				else {
-					throw new AssetProtocolError($"Invalid result type in server response. Type value sent: {type}");
+					throw new AssetProtocolError($"Invalid result type in client message. Type value sent: {type}");
 				}
 			}
 
-			if (!_assetIdIsReady && _bytesRead >= UUID_TAG_LOCATION + UUID_LEN) {
-				var idString = Encoding.ASCII.GetString(data, UUID_TAG_LOCATION, UUID_LEN);
+			if (!_assetIdIsReady && _dataHeaderWritePointer >= UUID_TAG_LOCATION + UUID_LEN) {
+				var idString = Encoding.ASCII.GetString(_dataHeader, UUID_TAG_LOCATION, UUID_LEN);
 				if (Guid.TryParse(idString, out var id)) {
 					AssetId = id;
 					_assetIdIsReady = true;
 				}
 				else {
-					throw new AssetProtocolError($"Invalid UUID in server response. Header summary: Type = {Type}, AssetId = {idString}");
+					throw new AssetProtocolError($"Invalid UUID in client message. Header summary: Type = {Type}, AssetId = {idString}");
 				}
 			}
 
-			if (_data == null && _bytesRead >= DATA_SIZE_MARKER_LOC + 4) {
-				var dataSize = Math.Max(0, InWorldz.Whip.Client.Util.NTOHL(data, DATA_SIZE_MARKER_LOC)); // No, you don't get to send me negative numbers.
+			if (_data == null && _dataHeaderWritePointer >= DATA_SIZE_MARKER_LOC + 4) {
+				var dataSize = Math.Max(0, InWorldz.Whip.Client.Util.NTOHL(_dataHeader, DATA_SIZE_MARKER_LOC)); // No, you don't get to send me negative numbers.
 				_data = new byte[dataSize];
 
-				var bytesToRead = Math.Min(dataSize, data.Length - HEADER_SIZE);
+				var bytesToRead = Math.Min(dataSize, data.Length - headerBytesInData); // No trying to send me more than you said you were sending!
 
 				if (dataSize > 0) {
 					// We are processing a packet of data that has the header.
-					Buffer.BlockCopy(data, HEADER_SIZE, _data, 0, bytesToRead);
+					Buffer.BlockCopy(data, headerBytesInData, _data, 0, bytesToRead);
 				}
 
 				_dataWritePointer = bytesToRead;
 			}
 			else if (_data != null) {
 				// Processing a pure data packet.
-				var bytesToRead = Math.Min(data.Length, _data.Length - _dataWritePointer);
+				var bytesToRead = Math.Min(data.Length, _data.Length - _dataWritePointer); // No trying to send me more than you said you were sending!
 
 				Buffer.BlockCopy(data, 0, _data, _dataWritePointer, bytesToRead);
 
---
+++ Source/LibWhipLruTests/Server/TestClientRequestMsg.cs
@@ -0,0 +1,221 @@
+﻿// TestClientRequestMsg.cs
+//
+// Author:
+//       Ricky Curtice <ricky@rwcproductions.com>
+//
+// Copyright (c) 2018 Richard Curtice
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Text;
+using LibWhipLru.Server;
+using NUnit.Framework;
+using static InWorldz.Whip.Client.ClientRequestMsg;
+
+namespace LibWhipLruTests.Server {
+	[TestFixture]
+	public static class TestClientRequestMsg {
+		private const short REQUEST_TYPE_LOC = 0;
+		private const short UUID_TAG_LOCATION = 1;
+		private const short UUID_LEN = 32;
+		private const short DATA_SIZE_MARKER_LOC = 33; // In big-endian format.
+		private const short HEADER_SIZE = 37;
+
+		#region Ctor (default as there's none defined)
+
+		[Test]
+		public static void TestClientRequestMsg_Ctor_DoesntThrow() {
+			Assert.DoesNotThrow(() => new ClientRequestMsg());
+		}
+
+		#endregion
+
+		#region AddRange
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_Null_ArgumentNullException() {
+			var msg = new ClientRequestMsg();
+
+			Assert.Throws<ArgumentNullException>(() => msg.AddRange(null));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_Empty_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+
+			Assert.DoesNotThrow(() => msg.AddRange(new byte[] { }));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_LessThanHeader_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+
+			var data = new byte[HEADER_SIZE - 1];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.DoesNotThrow(() => msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_AfterHeaderOneByte_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+			data[DATA_SIZE_MARKER_LOC + 3] = 1;
+			msg.AddRange(data);
+
+			Assert.DoesNotThrow(() => msg.AddRange(new byte[] { 0 }));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_AfterHeader1OneByte_True() {
+			var msg = new ClientRequestMsg();
+
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+			data[DATA_SIZE_MARKER_LOC + 3] = 1;
+			msg.AddRange(data);
+
+			Assert.IsTrue(msg.AddRange(new byte[] { 0 }));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_AfterHeader2OneByte_False() {
+			var msg = new ClientRequestMsg();
+
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+			data[DATA_SIZE_MARKER_LOC + 3] = 2;
+			msg.AddRange(data);
+
+			Assert.IsFalse(msg.AddRange(new byte[] { 0 }));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_CompleteHeaderNoData_True() {
+			var msg = new ClientRequestMsg();
+
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.IsTrue(msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_WrongHeaderId_AssetProtocolError() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = 0; // Anything not in range of RequestType.
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.Throws<InWorldz.Whip.Client.AssetProtocolError>(() => msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_PartialHeader_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE - 1];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.DoesNotThrow(() => msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_PartialHeader_False() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE - 1];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.IsFalse(msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_PartialHeaderCompleted_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+			var data1 = new byte[HEADER_SIZE - 1];
+			data1[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data1, UUID_TAG_LOCATION, UUID_LEN);
+			msg.AddRange(data1);
+
+			var data2 = new byte[1];
+			data2[0] = 0;
+			Assert.DoesNotThrow(() => msg.AddRange(data2));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_PartialHeaderCompleted_True() {
+			var msg = new ClientRequestMsg();
+			var data1 = new byte[HEADER_SIZE - 1];
+			data1[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data1, UUID_TAG_LOCATION, UUID_LEN);
+			msg.AddRange(data1);
+
+			var data2 = new byte[1];
+			data2[0] = 0;
+			Assert.IsTrue(msg.AddRange(data2));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRange_CompleteHeaderNoData_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.DoesNotThrow(() => msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRangeCompleteHeaderNoData_True() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+
+			Assert.IsTrue(msg.AddRange(data));
+		}
+
+		[Test]
+		public static void TestClientRequestMsg_AddRangeCompleteHeaderNegativeDate_DoesntThrow() {
+			var msg = new ClientRequestMsg();
+			var data = new byte[HEADER_SIZE];
+			data[REQUEST_TYPE_LOC] = (byte)RequestType.TEST;
+			Buffer.BlockCopy(Encoding.ASCII.GetBytes(Guid.NewGuid().ToString("N")), 0, data, UUID_TAG_LOCATION, UUID_LEN);
+			data[DATA_SIZE_MARKER_LOC] = 0xFF;
+			data[DATA_SIZE_MARKER_LOC + 1] = 0xFF;
+			data[DATA_SIZE_MARKER_LOC + 2] = 0xFF;
+			data[DATA_SIZE_MARKER_LOC + 3] = 0xFF;
+
+			Assert.DoesNotThrow(() => msg.AddRange(data));
+		}
+
+		#endregion
+
+
+	}
+}
